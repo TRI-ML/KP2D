6b9a61090c7b736ac2af8af8ee03c25322c049de
thomas <thomas.rueegg98@bluewin.ch>
2022-04-21 13:42:33 +0200

    add more ros functionalities

--- ros_test.py
+++ ros_test.py
@@ -1,19 +1,28 @@
 from rosbags.rosbag2 import Reader
+from rosbags.rosbag1 import Reader as Reader1
 from rosbags.serde import deserialize_cdr
 import numpy as np
 import cv2
 import os
-
+from rosbags.serde import deserialize_cdr, ros1_to_cdr
 imgs = []
 data_path = r"D:\MachineLearning\SonarData\SonarDataSets\Real"
-rosbag_path = r'C:\Users\Dr. Paul von Immel\Downloads\rosbag2_2022_04_06-11-41_16-20220408T162332Z-001\rosbag2_2022_04_06-11-41_16'
-
+rosbag_path = r'C:\Users\Dr. Paul von Immel\Downloads\rosbag2_2022_04_06-11-27_20-20220408T125835Z-001\rosbag2_2022_04_06-11-27_20'
 
 folderName = os.path.basename(rosbag_path)
 data_path_extracted = os.path.join(data_path, folderName)
 
 
+def get_xyz():
+    with Reader1(r"C:\Users\Dr. Paul von Immel\Downloads\2022-04-06-11-25-30.bag") as reader:
+        # topic and msgtype information is available on .connections dictionary
+        for connection in reader.connections.values():
+            print(connection.topic,'|', connection.msgtype)
 
+        for connection, timestamp, rawdata in reader.messages():
+            if connection.topic == '/waterlinked/acoustic_position/raw':
+                 msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
+                 print(msg)
 
 
 def rosbag_to_jpg(rosbag_path, data_path_extracted):
@@ -28,6 +37,16 @@ def rosbag_to_jpg(rosbag_path, data_path_extracted):
             cv2.imwrite(os.path.join(data_path_extracted, filename), img_reshaped)
             i += 1
 
+def rosbag_inv(rosbag_path):
+
+    with Reader(rosbag_path) as reader:
+        for connection in reader.connections.values():
+            print(connection.topic,'|', connection.msgtype)
+
+        for connection, timestamp, rawdata in reader.messages(connections=reader.connections.values()):
+            msg = deserialize_cdr(rawdata, connection.msgtype)
+            print(msg.header.stamp.sec)
+
 
 def load_all_imgs(data_path_extracted):
     images = []
@@ -38,11 +57,11 @@ def load_all_imgs(data_path_extracted):
             images.append(img)
     return np.asarray(images)
 
-imgs = load_all_imgs(r"D:\MachineLearning\SonarData\SonarDataSets\Real\rosbag2_2022_04_06-11-27_20")
-
-mean = np.mean(imgs,axis=0)
-for i in imgs:
-    cv2.imshow("minus mean", (i - mean*2)/255.)
-    cv2.imshow("i", i/255.)
-    cv2.imshow("mean", mean*4/255.)
-    cv2.waitKey(0)
+#imgs = load_all_imgs(r"D:\MachineLearning\SonarData\SonarDataSets\Real\rosbag2_2022_04_06-11-27_20")
+get_xyz()
+#rosbag_inv(rosbag_path)
+# for i in imgs:
+#     cv2.imshow("minus mean", (i - mean*2)/255.)
+#     cv2.imshow("i", i/255.)
+#     cv2.imshow("mean", mean*4/255.)
+#     cv2.waitKey(0)
